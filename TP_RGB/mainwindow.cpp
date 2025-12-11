ca257a3c69af7d982970c22e4bf20d7e6b7bdb95
Theotime Faure <theotimefaure@free.fr>
2025-12-11 09:38:19 +0100

    Changement de couleur

--- TP_RGB/mainwindow.cpp
+++ TP_RGB/mainwindow.cpp
@@ -1,14 +1,80 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    this->init();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_spinBox_Red_valueChanged(int value)
+{
+    ui->verticalSlider_Red->setValue(value);
+}
+
+void MainWindow::on_verticalSlider_Red_valueChanged(int value)
+{
+    ui->spinBox_Red->setValue(value);
+}
+
+void MainWindow::on_spinBox_Green_valueChanged(int value)
+{
+    ui->verticalSlider_Green->setValue(value);
+}
+
+void MainWindow::on_verticalSlider_Green_valueChanged(int value)
+{
+    ui->spinBox_Green->setValue(value);
+}
+
+void MainWindow::on_spinBox_Blue_valueChanged(int value)
+{
+    ui->verticalSlider_Blue->setValue(value);
+}
+
+void MainWindow::on_verticalSlider_Blue_valueChanged(int value)
+{
+    ui->spinBox_Blue->setValue(value);
+}
+
+void MainWindow::RGB_Adjust()
+{
+    QPalette palette;
+    ui->label_Color->setAutoFillBackground(true);
+    palette.setColor(QPalette::Window, QColor(QColor(ui->spinBox_Red->value(), ui->spinBox_Green->value(), ui->spinBox_Blue->value())));
+    ui->label_Color->setPalette(palette);
+}
+
+void MainWindow::init()
+{
+    ui->verticalSlider_Red->setStyleSheet("QSlider::handle:veritcal { background: qlineargradient(x1:0, y1:0, x2:1, y2:1, stop:0 white, stop:1 red); border: 1px solid #999999; border-radius: 5px;}");
+    ui->verticalSlider_Green->setStyleSheet("QSlider::handle:veritcal { background: qlineargradient(x1:0, y1:0, x2:1, y2:1, stop:0 white, stop:1 green); border: 1px solid #999999; border-radius: 5px;}");
+    ui->verticalSlider_Blue->setStyleSheet("QSlider::handle:veritcal { background: qlineargradient(x1:0, y1:0, x2:1, y2:1, stop:0 white, stop:1 blue); border: 1px solid #999999; border-radius: 5px;}");
+
+    ui->verticalSlider_Red->setRange(0, 255);
+    ui->verticalSlider_Green->setRange(0, 255);
+    ui->verticalSlider_Blue->setRange(0, 255);
+
+    ui->spinBox_Red->setRange(0, 255);
+    ui->spinBox_Green->setRange(0, 255);
+    ui->spinBox_Blue->setRange(0, 255);
+
+    connect(ui->spinBox_Red, SIGNAL(valueChanged(int)), this, SLOT(RGB_Adjust()));
+    connect(ui->spinBox_Green, SIGNAL(valueChanged(int)), this, SLOT(RGB_Adjust()));
+    connect(ui->spinBox_Blue, SIGNAL(valueChanged(int)), this, SLOT(RGB_Adjust()));
+
+    ui->spinBox_Red->setValue(255);
+    ui->spinBox_Green->setValue(255);
+    ui->spinBox_Blue->setValue(255);
+
+}
+
--- TP_RGB/mainwindow.h
+++ TP_RGB/mainwindow.h
@@ -17,6 +17,23 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_spinBox_Red_valueChanged(int value);
+
+    void on_verticalSlider_Red_valueChanged(int value);
+
+    void on_spinBox_Green_valueChanged(int value);
+
+    void on_verticalSlider_Green_valueChanged(int value);
+
+    void on_spinBox_Blue_valueChanged(int value);
+
+    void on_verticalSlider_Blue_valueChanged(int value);
+
+    void RGB_Adjust();
+
+    void init();
+
 private:
     Ui::MainWindow *ui;
 };
